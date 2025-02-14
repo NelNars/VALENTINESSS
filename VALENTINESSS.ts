531fae9b5a07fca8d34f4065a17246d08af04f2d
NelNars <Neilnvcc@gmail.com>
2025-02-14 13:32:53 +0800

    Add files via upload

--- VALENTINESSS.ts
+++ VALENTINESSS.ts
@@ -1,16 +1,17 @@
-Function rejectlove() {
-    If (no count <message.length) {
-        messageText.innerText = Message [no count];
-        no Count++;
-        noButton.style.transform = scale(${1 - no Count * 0.1})';
-        yesButton.style.transform = scale(${1 + no Count * 0.1})';
+function rejectLove() {
+    if (noCount < message.length) {
+        messageText.innerText = message[noCount];
+        noCount++;
+        noButton.style.transform = `scale(${1 - noCount * 0.1})`;
+        yesButton.style.transform = `scale(${1 + noCount * 0.1})`;
     }
 }
-Function accept love() {
-    Document.getElementById(Valentine).innerHTML = '
-        <img src="Https://media1.tenor.com/m/aEWN44So2ckAAAAC/kiss-kisses.gif" class="gif">
-        <div class= "question">YAAAAY HEART HEART!! HAHAHAHAHA<div>
-    ';
+
+function acceptLove() {
+    document.getElementById('Valentine').innerHTML = `
+        <img src="https://media1.tenor.com/images/aEWN44So2ckAAAAC/kiss-kisses.gif" class="gif">
+        <div class="question">YAAAAY HEART HEART!! HAHAHAHAHA<div>
+    `;
     launchConfetti();
     startHeartRain();
 }
